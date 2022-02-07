efc80199adfb2fdfc231be06185714ad001c0b56
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2022-02-06 22:31:33 -0800

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<script>alert("Hello World, Iam an alert box!")
+
+   function My_First_Function() {
+     var String = "Kiss me, I'm Irish!";
+	 var resulkt = String.fontcolor("green");
+	 document.getElementByID("Irish").innerHTML = result}
+	 
+</script>
+</html>
